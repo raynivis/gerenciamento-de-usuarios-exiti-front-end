f12b1e180613967ecc0f8277e22b57bfc7b7f5f7
uliaray <154099929+raynivis@users.noreply.github.com>
2025-11-02 13:22:06 -0300

    feat: adicionando .env

--- usuario-gerenciador-react/src/services/usuario.service.ts
+++ usuario-gerenciador-react/src/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { UsuarioResponse } from "../models/usuario.model";
 
-const API_URL = "http://localhost:8080";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export interface UsuarioFilters {
      search?: string;
